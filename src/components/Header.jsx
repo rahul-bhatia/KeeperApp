689546e119f745f796b84a906774a201c6be045b
unknown <rahul.1517.bhatia@gmail.com>
2020-11-20 10:23:21 +0530

    Final commit

---
+++ src/components/Header.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import FilterDramaIcon from '@material-ui/icons/FilterDrama';
+
+function Header() {
+  return (
+    <header>
+      <h1>{<FilterDramaIcon/>} Keeper</h1>
+    </header>
+  );
+}
+
+export default Header;
