28271f8838622ae41ff35b230ae0494507961b0c
unknown <rahul.1517.bhatia@gmail.com>
2020-11-20 10:44:01 +0530

    Updates

---
+++ precache-manifest.963dc38ddcb14bf9e6a218537ec4f277.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e8e891db343b7dc6ea04b17265a286c0",
+    "url": "/KeeperApp/index.html"
+  },
+  {
+    "revision": "d19307a25bcc9bd889d9",
+    "url": "/KeeperApp/static/js/2.a3df3754.chunk.js"
+  },
+  {
+    "revision": "a71e5d5d63e0fa952b37",
+    "url": "/KeeperApp/static/js/main.f3858293.chunk.js"
+  },
+  {
+    "revision": "ad92294fb1599dc8ff6b",
+    "url": "/KeeperApp/static/js/runtime-main.ff26de38.js"
+  }
+]);
